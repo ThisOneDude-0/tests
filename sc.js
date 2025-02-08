804c952ca984e9489d9b57b737090af048897369
This1Dude0 <this1dude.0@gmail.com>
2025-02-07 18:40:03 -0700

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,5 @@
+console.log("connected")
+const canv = document.getElementById("screen");
+const screen = canv.getContext("2d");
+screen.fillStyle = "#afafaf"
+screen.fillRect(0,0,window.innerWidth,window.innerHeight)
